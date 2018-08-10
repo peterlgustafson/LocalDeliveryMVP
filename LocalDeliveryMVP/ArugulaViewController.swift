9c91d241bd2aadf915b4e0a3b2277b5f91b89d00
Peter Gustafson <petergustafson@peters-macbook-air.local>
2018-08-10 15:33:41 -0500

    Added app logo images

--- LocalDeliveryMVP/ArugulaViewController.swift
+++ LocalDeliveryMVP/ArugulaViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ArugulaViewController: UIViewController {
+    
+    //Variable to store User Order Dictionary
+    var userOrder: [String: Double] = [:]
+    
+    @IBAction func arugulaAdded(_ sender: Any) {
+        userOrder["Arugula"] = 4.99
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +29,24 @@ class ArugulaViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func checkOutButtonPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: "goToShoppingCartFromArugula", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goToShoppingCartFromArugula" {
+            let destinationVC = segue.destination as! ShoppingCartViewController
+            
+            destinationVC.userOrder = userOrder
+            
+        }
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
